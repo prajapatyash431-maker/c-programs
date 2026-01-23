a374efa94ac17103e24718b0180806282dc79cb1
yash prajapat <prajapatyash431@gmail.com>
2026-01-23 18:38:50 +0530

    variables declaration

---
+++ basics/variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // Variable declaration
+
+    int a = 10;    // Integer variable
+    float b = 5.5; // Floating point variable
+    char c = 'A';  // Character variable
+
+    // Print variable values
+    printf("Integer value: %d\n", a);
+    printf("Float value: %f\n", b);
+    printf("Character value: %c\n", c);
+
+    return 0;
+}
