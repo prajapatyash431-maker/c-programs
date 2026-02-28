5c0bdf403caf4bd48a7da176f81f74e5752229a9
yash prajapat <prajapatyash431@gmail.com>
2026-02-28 14:45:09 +0530

    Implemented program to print ASCII value of a character

---
+++ basics/ascii_value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // C program to print ASCII value of a character
+
+    char a;
+    printf("Enter character : ");
+    scanf("%c", &a);
+
+    printf("ASCII value of %c is : %d", a, a);
+
+    return 0;
+}
