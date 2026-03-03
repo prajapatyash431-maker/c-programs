716f8cd1ce21c39e3e69c79890e441845668647f
yash prajapat <prajapatyash431.in>
2026-03-04 00:08:11 +0530

    Program to print character A to Z

---
+++ loops/print_char_A-Z.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Program to print character A - Z, Using loop.
+    for (char ch = 'A'; ch <= 'Z'; ch++)
+
+    {
+        printf("%c \n", ch);
+    }
+
+    return 0;
+}
