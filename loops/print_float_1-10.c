965bc7f638f6c87ddd8376bf8c49dccfe6078c92
yash prajapat <prajapatyash431@gmail.com>
2026-03-04 00:09:41 +0530

    Program to print floting point no. 1.0 to 10.0

---
+++ loops/print_float_1-10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // Program to print flooting point value 1.0 - 10.0, Using loop.
+    for (float i = 1.0; i <= 10.0; i++)
+    {
+        printf("%.1f \n", i);
+    }
+
+    return 0;
+}
