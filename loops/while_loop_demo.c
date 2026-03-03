2d1100da947b8d6b48e8f1e11ac7925fe7539cfa
yash prajapat <prajapatyash431.in>
2026-03-04 00:06:21 +0530

    Practice program of while loop

---
+++ loops/while_loop_demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // Program to print no (1 to 10) by using while loop.
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
