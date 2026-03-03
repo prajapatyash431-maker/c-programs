dd1bc33dd95340265ac9c98684ddc50b6040c974
yash prajapat <prajapatyash431.in>
2026-03-04 00:05:57 +0530

    Program to print hello world imfinite times

---
+++ loops/infinite_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Infinite loop
+    // program to print hello world infinite times.
+    for (int i = 1;; i++)
+    {
+        printf("Hello World \n");
+    }
+
+    return 0;
+}
