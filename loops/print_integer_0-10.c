bef806d8eaab3781fa2ffebff60b195dd27d15f5
yash prajapat <prajapatyash431@gmail.com>
2026-03-04 00:10:17 +0530

    Program to print integer no. 1 to 10

---
+++ loops/print_integer_0-10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // Program to print no. (0 to 10) by using for loop.
+    for (int i = 0; i <= 10; i = i + 1)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
