9fe45b055d16050d6d76fe728d2e79f9b79e5be2
yash prajapat <prajapatyash431@gmail.com>
2026-05-30 13:47:15 +0530

    Program to calculate yearly salary by mounthly salaty

---
+++ basics/yearly_salary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // Program to calculate yearly salary from mounthly salary.
+    int mounthly_salary;
+    int yearly_amount;
+
+    printf("Enter mounthly salary : ");
+    scanf("%d", &mounthly_salary);
+
+    yearly_amount = mounthly_salary * 12;
+
+    printf("Yearly amount of salary : %d", yearly_amount);
+
+    return 0;
+}
